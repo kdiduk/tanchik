4ea0b599c96fb27ccbde8185ef3a9d5989baf91b
Kirill Diduk <kirill.diduk@mail.ru>
2019-11-09 22:03:06 +0100

    Cosmetic: comments for `ZORG` command

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# origin of the game binary
-ZORG=25000
+# Origin of the game binary. Default value is 32768.
+# For other values, add `-zorg=${ZORG}` option to `zcc` command.
+ZORG=32768
 
 mkdir -p bin
 cd bin
 
 zcc +zx -startup=31 -clib=sdcc_iy @../tanchik.lst \
-	-zorg=${ZORG} -pragma-include:../zpragma.inc -o tanchik
+	-pragma-include:../zpragma.inc -o tanchik
 
 appmake +zx -b tanchik_CODE.bin -o tanchik.tap \
 	--blockname tanchik --org ${ZORG}
