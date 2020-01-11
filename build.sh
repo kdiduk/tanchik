49adc4c0f4cbd7bca955ffe51d8a5463787b83af
Kirill Diduk <kirill.diduk@mail.ru>
2020-01-11 22:31:15 +0100

    Cosmetic
    Add comment about `--audio` flag.
    Output string when build finished.

--- build.sh
+++ build.sh
@@ -10,8 +10,10 @@ cd bin
 zcc +zx -Wall -startup=31 -clib=sdcc_iy @../tanchik.lst \
 	-I../include -pragma-include:../zpragma.inc -o tanchik
 
+# Add option `--audio` to produce wav-tape
 appmake +zx -b tanchik_CODE.bin -o tanchik.tap \
 	--blockname tanchik --org ${ZORG}
 
 cd ..
 
+echo "Build finished"
